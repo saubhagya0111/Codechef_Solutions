744b1c0f42bf314a78c1a93f3d6880395131d8c7
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-08-29 00:18:00 +0530

    Create PROGLANG.cpp

---
+++ LTIME99C/PROGLANG.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int c = 0;
+        if (t <= 10 && n <= 100)
+        {
+            int a[n];
+            for (int i = 0; i < n; i++)
+            {
+                cin >> a[i];
+            }
+            // for (int i = 0; i < n; i++)
+            {
+                cout << c << endl;
+            }
+        }
+    }
+
+    return 0;
+}
